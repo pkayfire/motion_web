c6f52b2d6946257c86682260ae96847fed44fce5
Peter Kim <peterkim45366@gmail.com>
2014-11-30 11:35:41 -0800

    added gzipping

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var compress = require('compression');
+
 var app = express();
 
 var router = express.Router();
@@ -7,8 +9,9 @@ router.use(function(req, res, next) {
   next();
 });
 
-global.setImmediate = global.setImmediate || process.nextTick.bind(process)
+global.setImmediate = global.setImmediate || process.nextTick.bind(process);
 
+app.use(compress());
 app.use(express.static(__dirname + '/dist/'));
 
 app.get('*', function(req, res){
